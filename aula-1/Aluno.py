b8ba7fd763de248c845d58fe7e6c2ea7e3dc945c
Jhuliano Maia <jhulianomaia@gmail.com>
2025-08-20 16:17:01 -0400

    Criando classe aluno

--- aula-1/Aluno.py
+++ aula-1/Aluno.py
@@ -6,17 +6,37 @@
 
 class Aluno:
     #Comentario em linha
+    #atributo de classe abaixo
     instituicaoEnsino: str
 
     #Construtor
-    def __init__(self, matricula: str, nome: str, idade: str):
+    #Os atributos de instância estão sempre como parametros do construtor
+    def __init__(self, matricula: int, nome: str, idade: int):
         #atribuição instancia
         #valores unico para cada instancia do objeto
         self.matricula = matricula
         self.nome = nome
         self.idade = idade
         self.instituicaoEnsino = "IFAM"
-        print("_____________________________________________________________")
-        print("Objeto Aluno criado com sucessp...")
-        print("_____________________________________________________________")
+        print("------------------------------------------------------------------")
+        print("Objeto Aluno criado com sucesso...")
+        print("------------------------------------------------------------------")
         
+    #Destruidor
+    def __del__(self):
+        print("------------------------------------------------------------------")
+        print("Objeto Aluno destruido com sucesso...")
+        print("------------------------------------------------------------------")
+
+    def __str__(self):
+        return (
+            f"------------------------------------------------------------------\n"
+            f"Objeto Aluno:\n"
+            f"Matrícula: {self.matricula}\n"
+            f"Nome: {self.nome}\n"
+            f"Idade: {self.idade}\n"
+            f"Instituição: {self.instituicaoEnsino}\n"
+            f"------------------------------------------------------------------"
+
+        )
+    
